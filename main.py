f483a0ae4569921636865f9932e1f6db0745d5bb
Zion Awino <awinozion85@gmail.com>
2023-09-10 13:31:29 +0200

    Added the basic UI and rules to the game.

--- main.py
+++ main.py
@@ -1,16 +1,24 @@
-# This is a sample Python script.
+def print_separator(ui_width, char='-'):
+    print(char * ui_width)
 
-# Press Shift+F10 to execute it or replace it with your code.
-# Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
+def print_centered_text(text, ui_width, char='-'):
+    formatted_text = f'{char} {text} {char}'
+    print(formatted_text.center(ui_width))
 
+ui_width = 50
 
-def print_hi(name):
-    # Use a breakpoint in the code line below to debug your script.
-    print(f'Hi, {name}')  # Press Ctrl+F8 to toggle the breakpoint.
+print_separator(ui_width)
+print_centered_text('BLACK JACK', ui_width)
+print_separator(ui_width)
 
-
-# Press the green button in the gutter to run the script.
-if __name__ == '__main__':
-    print_hi('PyCharm')
-
-# See PyCharm help at https://www.jetbrains.com/help/pycharm/
+underStand = input("Do you know the rules of playing Black jack?(yes/no) ").lower()
+if underStand == "yes":
+    print("Tha is great. We will now continue") #Have a fuction here that continues to the game.
+elif underStand == "no":
+    print("\nRules of Blackjack:")
+    print("1. The goal is to beat the dealer without going over 21.")
+    print("2. You can 'hit' to draw more cards or 'stand' to keep your current total.")
+    print("3. Face cards are worth 10, Aces are worth 11 (or 1 if it helps you).")
+    print("4. If you go over 21, you lose ('bust').")
+    print("5. The dealer must hit until they have 17 or higher.")
+    print("6. You can 'quit' at any time to end the game.")
